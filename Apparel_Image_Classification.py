b9ef7a97cd714c557b843685b65fef5e95616d66
Riya Jain <46837551+riya1103@users.noreply.github.com>
2019-09-02 23:02:37 +0530

    Update Apparel_Image_Classification.py

--- Apparel_Image_Classification.py
+++ Apparel_Image_Classification.py
@@ -56,6 +56,13 @@ X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=42, test_
 classifier.fit(X_train,y_train,epochs=10,,validation_data=(X_test,y_test))
 
 
+y_pred = classifier.predict_classes(test_X)
+
+
+df=pd.DataFrame()
+df['label']= y_pred
+df.to_csv('submission.csv')
+
 
 
 
